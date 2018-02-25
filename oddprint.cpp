d4001b81291d303e7910a7d0a3df20de2d95fdc1
SangaviPandian <36364726+SangaviPandian@users.noreply.github.com>
2018-02-25 00:49:25 -0800

    Create oddprint.cpp

---
+++ oddprint.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int num,m =20,n=40;
+    cout<<"Print Odd Numbers in a given range m to n:";
+    for (num = m; num <= n; num++)
+    {
+    	if (num % 2 == 1)
+    	cout<<num<<endl;
+    	}
+	return 0;
+}
