ffd9e20adf8e82cc848b08366d7b14c00371b2c6
SangaviPandian <36364726+SangaviPandian@users.noreply.github.com>
2018-02-28 10:07:18 +0530

    Create arithmetic.cpp

---
+++ arithmetic.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n,a,d,ap;
+	cout<<"enter the value of n,a,d";
+	cin>>n;
+	cin>>a;
+	cin>>d;
+	ap=(n/2(2*a+(n-1)*d));
+	cout<<ap;
+	return 0;
+}
