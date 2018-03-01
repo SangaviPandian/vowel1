8b1671229763214d3d82cf18dfdfa93589e1e958
SangaviPandian <36364726+SangaviPandian@users.noreply.github.com>
2018-03-01 14:05:08 +0530

    Create index.cpp

---
+++ index.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n=10,a[]={1,2,3,4,5,6,7,8,9,10};
+	for(int i=0;i<=n;i++)
+	{
+		cout<<a[i]<<"index="<<endl<<i<<endl;
+	}
+	return 0;
+}
