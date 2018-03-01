1f9a940de2fb5a7cd7294657e6957d1ac173db27
SangaviPandian <36364726+SangaviPandian@users.noreply.github.com>
2018-03-01 08:06:03 -0800

    Create countwords.cpp

---
+++ countwords.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main()
+{
+    using std::string;
+    using std::cin;
+    using std::cout;    
+
+    string aString;
+    char tempChar;
+    int i=0;
+    while (!cin.eof())
+    {
+        cin.get(tempChar);
+        if(tempChar==' ')
+        aString += tempChar;
+        i++;
+    }
+    
+    cout << aString;
+    cout<<aString.length()+1;
+    
+    return 0;
+}
