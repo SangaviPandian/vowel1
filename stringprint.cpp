fbcda6965d97635cf839b13aa40e8b2bca7c1467
SangaviPandian <36364726+SangaviPandian@users.noreply.github.com>
2018-03-02 16:22:41 +0530

    Create stringprint.cpp

---
+++ stringprint.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+    int k,i;
+	string n;
+	cout<<"enter the string";
+	cin>>n;
+	cout<<"enter how many number of times to print";
+	cin>>k;
+	for(i=0;i<k;i++)
+	{
+	    cout<<n<<endl;
+	    
+	}
+	
+
+    return 0;
+}
