d3de75fc9fac7c11c2bb5632db90b8db5968e3ed
SangaviPandian <36364726+SangaviPandian@users.noreply.github.com>
2018-03-02 10:07:44 +0530

    Create countnum.c

---
+++ countnum.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main() 
+{
+	char str[100];
+	int i,counter=0;
+	printf("enter the sentence to count the number of numeric charaters in the sentence:");
+	scanf("%[^\n]s",str);
+	for(i=0;str[i]!='\0';i++)
+	{
+		if(str[i]>='0' && str[i]<='9')
+		{
+			counter=counter+1;
+		}
+	}
+	printf("\nthe number of numeric in the sentence is:%d",counter);
+	return 0;
+}
