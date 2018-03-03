166c6859b55dc0122f1ba1054b5ce3d862d7efe0
SangaviPandian <36364726+SangaviPandian@users.noreply.github.com>
2018-03-03 10:22:04 +0530

    Create range.cpp

---
+++ range.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cout<<"enter the number"<<endl;
+	cin>>n;
+	if(n>=1&&n<=10)
+	{
+		cout<<"yes"<<endl;
+	}
+	else
+	{
+		cout<<"no"<<endl;
+	}
+	return 0;
+}
