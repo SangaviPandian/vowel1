02374395f57d17aa68a1d718fe0c8b11b427df87
SangaviPandian <36364726+SangaviPandian@users.noreply.github.com>
2018-03-08 07:27:57 -0800

    Create sum.cpp

---
+++ sum.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int n1,n2,sum;
+cout<<"\n enter the first number:"<<endl;
+	cin>>n1;
+cout<<"\n enter the second number:"<<endl;
+	cin>>n2;
+	sum=n1+n2;
+	cout<<sum<<endl;
+	return 0;
+}
