e734237fa168fd26527b6ccb78efd4bf5d193299
SangaviPandian <36364726+SangaviPandian@users.noreply.github.com>
2018-03-08 07:51:58 -0800

    Create wordnum.cpp

---
+++ wordnum.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+ using namespace std;
+int main() 
+{
+	int num;
+	cout<<"\n enter the number from 1 to 10";
+	cin>>num;
+	switch(num)
+	{
+		case 1:
+		cout<<"\none";
+		break;
+		case 2:
+		cout<<"\ntwo";
+		break;
+		case 3:
+		cout<<"\nthree";
+		break;
+		case 4:
+		cout<<"\nfour";
+		break;
+		case 5:
+		cout<<"\nfive";
+		break;
+		case 6:
+	cout<<"\nsix";
+		break;
+		case 7:
+		cout<<"\nseven";
+		break;
+		case 8:
+		cout<<"\neight";
+		break;
+		case 9:
+		cout<<"\nnine";
+		break;
+		case 10:
+		cout<<"\nten";
+		break;
+		default:
+		cout<<"\n enter numbers from 1 to 10";
+	}
+ 
+	return 0;
+}
