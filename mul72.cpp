a246b8faf24261fceadb2cce2fe101993a177cac
SangaviPandian <36364726+SangaviPandian@users.noreply.github.com>
2018-03-15 09:42:04 -0700

    Rename mul7.cpp to mul72.cpp

---
+++ mul72.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+	int n;
+	cout<<"enter the number:";
+	cin>>n;
+	if(n%7==0)
+	{
+		cout<<"\n yes";
+	}
+	else
+	{
+		cout<<"\n no";
+	}
+	return 0;
+}
