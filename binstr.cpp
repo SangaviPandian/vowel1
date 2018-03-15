af6b522513cdf02adcc0acf35cd2a2b2377a8361
SangaviPandian <36364726+SangaviPandian@users.noreply.github.com>
2018-03-15 07:37:54 -0700

    Create binstr.cpp

---
+++ binstr.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include<string.h>
+using namespace std;
+int main()
+
+{
+	char a[10];
+	cin>>a;
+	int i,flag=0,m;
+	m=strlen(a);
+	for(i=0;i<m;i++)
+	{
+		if(a[i]=='1' || a[i]=='0')
+		{
+			flag=1;
+		}
+		else
+{
+
+			flag=0;
+			break;
+		}
+	}
+	if(flag==1)
+		cout<<"yes";
+		else
+		cout<<"no";
+}
