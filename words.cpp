9df951c79ed3015bb5664e929dcc4196668f5ce8
SangaviPandian <36364726+SangaviPandian@users.noreply.github.com>
2018-03-15 09:04:22 -0700

    Create words.cpp

---
+++ words.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+    char a[100];
+    int i,count=1;
+    
+    cout<<"Enter a string:";
+    cin.get(a,20);
+    
+    for(i=0;a[i]!='\0';++i)
+    {
+        if(a[i]==' ')
+            count++;
+    }
+    
+    cout<<"\nThere are "<<count+1
+    <<" words in the given string";
+ 
+    return 0;
+}
