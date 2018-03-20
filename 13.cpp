bebc61a666db920b4546341da3a634d3e04f1331
SangaviPandian <36364726+SangaviPandian@users.noreply.github.com>
2018-03-20 07:51:03 -0700

    Create 13.cpp

---
+++ 13.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+
+int main() 
+
+{
+
+	int n;
+
+	cout<<"enter the number:";
+
+	cin>>n;
+
+	if(n%13==0)
+
+	{
+
+	cout<<"\n yes";
+
+	}
+
+	else
+
+	{
+
+		cout<<"\n no";
+
+	}
+
+	return 0;
+
+}
